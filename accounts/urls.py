3f099b3a8f07a10161f71f5055c03afe06c13e61
w00ye0l <dnduf158@gmail.com>
2022-10-14 11:21:01 +0900

    accounts/update

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("signup/", views.signup, name="signup"),
     path("login/", views.login, name="login"),
     path("<int:user_pk>/", views.detail, name="detail"),
+    path("update/", views.update, name="update"),
 ]
